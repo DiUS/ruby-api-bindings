d0ed7dc270bac8758f64cf4cb752ec5e451c3f48
Tal Rotbart <redbeard@gmail.com>
2011-11-17 13:52:07 +1100

    Added optional parameter encoding

--- lib/springsense-ruby/disambiguator.rb
+++ lib/springsense-ruby/disambiguator.rb
@@ -32,13 +32,23 @@ class Disambiguator
   end
 
   def call_service(text)
+    params = {}
+    params[:api_key] = api_key unless api_key.nil?
+    params[:customer_id] = customer_id unless customer_id.nil?
+    
     Net::HTTP.start(@address, @port) do |client|
       client.open_timeout = 120
       client.read_timeout = 120
-      client.post("/disambiguate?customer_id=#{self.customer_id}&api_key=#{self.api_key}", text).body
+      client.post("/disambiguate?#{self.encode_parameters(params)}", text).body
     end
   rescue => e
     raise "Disambiguator #{@address}:#{@port} - Error while disambiguating '#{text}': #{e}"
+  end
+
+  def encode_parameters(parameters = {})
+    parameters.map do | key, value |
+      "#{key.to_s}=#{URI.escape(value.to_s)}"
+    end.join('&')
   end
 
 end
